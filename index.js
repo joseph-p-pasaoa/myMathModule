657acaeb13a1925de47672c123fab457e839d5ec
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-05 21:00:40 -0500

    added div to index

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
   This is a collaboratively made Math module with support for basic math operations.
   Feel free to use it for you basic math needs.
 */
+const div = require('./divide.js');
 const Math = {
   PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
 }
 
 module.exports = Math
+module.exports = div;
