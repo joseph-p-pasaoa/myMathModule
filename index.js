7a87729bd8e9009f39f085cb1d70aee36482fed3
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-05 21:07:18 -0500

    fixed and new commit added div to index

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 */
 const div = require('./divide.js');
 const Math = {
-  PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+  PI: 3.141592653589793, // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+  div: div
 }
 
 module.exports = Math
-module.exports = div;
