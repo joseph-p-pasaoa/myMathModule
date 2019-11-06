5d99db9f3dad487788a26b688c3e875d396f88ec
Joseph P. Pasaoa <52214348+joseph-p-pasaoa@users.noreply.github.com>
2019-11-05 21:26:42 -0500

    resolved conflict and fixed error
    @DouglasMacKrell @KathyPuma @Bpmauge 
    please approve

--- index.js
+++ index.js
@@ -6,10 +6,6 @@
 */
 
 
-const Math = {
-  PI: 3.141592653589793,
-   // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
-=======
 const sum = require('./sum.js');
 const sub = require('./sub.js');
 const mul = require('./mul.js');
